c01fd67c796337ff2f6dfa0e662dbe1e8b5f4336
MPAA-DOM\KreymerI <MPAA-DOM\KreymerI>
2013-08-27 13:20:30 -0700

    Added CaraOnline

---
+++ CaraOnlineForms/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaraEntites;
+using Repository;
+
+namespace CaraOnlineForms.Controllers
+{
+    public class AccountController : Controller
+    {
+       
+        public ActionResult Registration()
+        {
+           
+            //var rep = new UserRepository();
+            //rep.IsEmailUnique("aaa");
+            return View(new User());
+        }
+
+    }
+}
---
+++ Repository/Repositories/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using CaraEntites;
+
+
+
+namespace Repository
+{
+    public class UserRepository: RepositoryBase
+    {
+
+        public bool IsEmailUnique(string email)
+        {
+            var _context = new CaraEntities();
+            var u = _context.Users.Where(x => x.EmailAddress == email).ToList();
+            return u.Count == 0;
+        }
+    }
+}
