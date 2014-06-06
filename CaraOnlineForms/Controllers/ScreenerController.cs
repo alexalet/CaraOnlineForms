7447c74dc34737f9452088df4e85116e839b6a06
MPAA-DOM\LevinAl <MPAA-DOM\LevinAl>
2014-06-06 09:22:23 -0700

    00

---
+++ CaraOnlineForms/Controllers/ScreenerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaraEntites;
+using Repository;
+using ViewModels;
+
+namespace CaraOnlineForms.Controllers
+{
+    public class ScreenerController : ControllerBase
+    {
+        //
+        // GET: /Screener/
+
+        public ActionResult Screener(int? filmId)
+        {
+            ScreenerViewModel model = new ScreenerViewModel();
+            model.ListOfAttributes = new ScreenerRepository().GetListOfAttributes();
+            model.ListOfGenres = new ScreenerRepository().GetListOfGenres();
+            model.ListOfFormats = new ScreenerRepository().GetListOfFormats();
+            int filmSubId = filmId ?? -1;
+            Screener screener = new ScreenerRepository().GetScreener(filmSubId);
+            if (screener != null)
+                model.Screener = screener;
+
+            return View();
+        }
+
+    }
+}
---
+++ CaraOnlineForms/ViewModels/ScreenerInfoViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CaraEntites;
+
+namespace ViewModels
+{
+    public class ScreenerInfoViewModel
+    {
+        public Screener Screener { get; set; }
+
+        public string Attributes
+        {
+            get
+            {
+                string res = "";
+                foreach (CaraEntites.Attribute a in Screener.Attributes)
+                {
+                    res += (res == "" ? "" : ", ") + a.Description;
+                }
+                return res;
+            }
+        }
+
+        public string Genres
+        {
+            get
+            {
+                string res = "";
+                foreach (ScreenerGenre g in Screener.ScreenerGenres)
+                {
+                    res += (res == "" ? "" : ", ") + g.Genre.Description;
+                }
+                return res;
+            }
+        }
+
+        public string PostScreeningAction
+        {
+            get
+            {
+                return (Screener.PostScreeningAction != null ? Screener.PostScreeningAction.Description : "");
+            }
+        }
+    }
+
+}
