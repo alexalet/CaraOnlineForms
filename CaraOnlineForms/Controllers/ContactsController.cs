73cb84e84566eca3f2d4fa5fd091c54f494b5220
MPAA-DOM\LevinAl <MPAA-DOM\LevinAl>
2014-06-19 15:22:52 -0700

    Worked on Contacts

--- CaraOnlineForms/Controllers/ContactsController.cs
+++ CaraOnlineForms/Controllers/ContactsController.cs
@@ -11,24 +11,21 @@ namespace CaraOnlineForms.Controllers
 {
     public class ContactsController : ControllerBase
     {
+        /// <summary>
+        /// Retrieves the list of all Certificate Mailing (Right Holder) Recipients info by the film Id
+        /// </summary>
+        /// <param name="filmId"></param>
+        /// <returns></returns>
         public ActionResult GetMailingList(int filmId)
         {
             List<CertificateMailing> mailingList = new ContactsRepository().GetMailingList(filmId);
-            List<CertificateMailingViewModel> model = new List<CertificateMailingViewModel>();
 
             mailingList.Add(new CertificateMailing { CertificateMailingId = -1, FilmSubmissionId = filmId, Suffix = "" });
-            for (int i=0;i<mailingList.Count;i++)
-            {
-                CertificateMailingViewModel certModel = new CertificateMailingViewModel { Recipient = mailingList[i], Counter = i };
-                model.Add(certModel);
-            }
-            return View("Contacts", model);
-
+            return View("Contacts", mailingList);
         }
 
-        public ActionResult SaveRecipient(int FilmSubmissionId, int CertificateMailingId, string FirstName, string LastName, string Suffix, string Title, string Company, string Address1, string Address2, string City, string State, string Zip, string Country, string Phone, string Fax, string Email)
+        public ActionResult SaveRecipient(int FilmSubmissionId, int CertificateMailingId, string FirstName, string LastName, string Suffix, string Title, string Company, string Address1, string Address2, string City, string State, string Zip, string Country, string Phone, string Fax, string Email,bool Original)
         {
-            List<CertificateMailingViewModel> model = new List<CertificateMailingViewModel>();
             CertificateMailing cert = new CertificateMailing { FilmSubmissionId = FilmSubmissionId, 
                 CertificateMailingId = CertificateMailingId, 
                 FirstName = FirstName, 
@@ -44,34 +41,24 @@ namespace CaraOnlineForms.Controllers
                 Country = Country, 
                 Phone = Phone, 
                 Fax = Fax, 
-                Email=Email };
+                Email=Email,
+                Original=Original
+            };
             List<CertificateMailing> mailingList = new ContactsRepository().SaveMailingRecipient(cert, this.User.UserId);
 
             mailingList.Add(new CertificateMailing { CertificateMailingId = -1, FilmSubmissionId = FilmSubmissionId, Suffix = "" });
-            for (int i = 0; i < mailingList.Count; i++)
-            {
-                CertificateMailingViewModel certMod = new CertificateMailingViewModel { Recipient = mailingList[i], Counter = i };
-                model.Add(certMod);
-            }
-            ViewBag.Message = "The Contact has been saved";
-            return View("Contacts", model);
-        }
 
+            ViewBag.Message = "The Contact has been saved";
+            return View("Contacts", mailingList);
+        }
 
         public ActionResult DeleteRecipient(int FilmSubmissionId,int CertificateMailingId)
         {
-            List<CertificateMailingViewModel> model = new List<CertificateMailingViewModel>();
             CertificateMailing cert = new CertificateMailing { FilmSubmissionId=FilmSubmissionId, CertificateMailingId = CertificateMailingId };
             List<CertificateMailing> mailingList = new ContactsRepository().DeleteMailingRecipient(cert, this.User.UserId);
             mailingList.Add(new CertificateMailing { CertificateMailingId = -1, FilmSubmissionId = cert.FilmSubmissionId, Suffix = "" });
             ViewBag.Message = "The Contact has been deleted";
-            for (int i = 0; i < mailingList.Count; i++)
-            {
-                CertificateMailingViewModel certMod = new CertificateMailingViewModel { Recipient = mailingList[i], Counter = i };
-                model.Add(certMod);
-            }
-            return View("Contacts", model);
+            return View("Contacts", mailingList);
         }
-
     }
 }
--- Repository/Repositories/ContactsRepository.cs
+++ Repository/Repositories/ContactsRepository.cs
@@ -10,16 +10,34 @@ namespace Repository
 {
     public class ContactsRepository:RepositoryBase
     {
+        /// <summary>
+        /// Retrieves the list of all existing recipients
+        /// for the current Title 
+        /// </summary>
+        /// <param name="filmSubmissionId"></param>
+        /// <returns></returns>
          public List<CertificateMailing> GetMailingList(int filmSubmissionId)
         {
-            return _context.CertificateMailings.Where(x => x.FilmSubmissionId == filmSubmissionId).ToList();
+            return _context.CertificateMailings.Where(x => x.FilmSubmissionId == filmSubmissionId).OrderBy(y => y.Original).ToList();
         }
 
+        /// <summary>
+         /// Retrieves the Certificate Mailing (Right Holder) Recipient info
+        /// </summary>
+        /// <param name="certMailingId"></param>
+        /// <returns></returns>
         public CertificateMailing GetCertificateMailingRecipient(int certMailingId)
         {
             return _context.CertificateMailings.FirstOrDefault(x => x.CertificateMailingId == certMailingId);
         }
 
+        /// <summary>
+        /// Creates the new or updates the existing Certificate Mailing (Right Holder) Recipient info and retrieves the list of all existing recipients
+        /// for the current Title
+        /// </summary>
+        /// <param name="cert"></param>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         public List<CertificateMailing> SaveMailingRecipient(CertificateMailing cert,int userId)
         {
             int filmSubmissionId = cert.FilmSubmissionId;
@@ -47,14 +65,22 @@ namespace Repository
                 certOld.Suffix = cert.Suffix;
                 certOld.Title = cert.Title;
                 certOld.Zip = cert.Zip;
+                certOld.Original = cert.Original;
                 certOld.ModifiedDate = DateTime.Now;
                 certOld.ModifiedBy = userId;
                 _context.SaveChanges();
             }
 
-            return _context.CertificateMailings.Where(x => x.FilmSubmissionId == filmSubmissionId).ToList();
+            return _context.CertificateMailings.Where(x => x.FilmSubmissionId == filmSubmissionId).OrderBy(y=>y.Original).ToList();
         }
 
+        /// <summary>
+        /// Deletes a specific Certificate Mailing (Right Holder) Recipient info and retrieves the list of all existing recipients
+        /// for the current Title
+        /// </summary>
+        /// <param name="cert"></param>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         public List<CertificateMailing> DeleteMailingRecipient(CertificateMailing cert, int userId)
         {
             int filmSubmissionId = cert.FilmSubmissionId;
@@ -62,7 +88,7 @@ namespace Repository
             _context.CertificateMailings.Remove(certOld);
             _context.SaveChanges();
 
-            return _context.CertificateMailings.Where(x => x.FilmSubmissionId == filmSubmissionId).ToList();
+            return _context.CertificateMailings.Where(x => x.FilmSubmissionId == filmSubmissionId).OrderBy(y => y.Original).ToList();
         }
     }
 }
